1c65f676643b3dea90526da654f7216b3ce549fb
Ryu Tae Hyun <ryu200112@gmail.com>
2026-02-15 22:58:32 +0900

    프로그래머스 레벨1: 최직, 가가글

---
+++ 류태현/week5/programmers_최소직사각형.js
@@ -0,0 +1,14 @@
+function solution(sizes) {
+  let maxW = 0;
+  let maxH = 0;
+
+  for (const [w, h] of sizes) {
+    const big = Math.max(w, h);
+    const small = Math.min(w, h);
+
+    if (big > maxW) maxW = big;
+    if (small > maxH) maxH = small;
+  }
+
+  return maxW * maxH;
+}
